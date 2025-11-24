85bacf793d81b8402f48f51453525f752a56a203
Oliver <oschi1303@gmail.com>
2025-11-24 20:24:57 +0100

    fancycore: Kick player if banned on join

--- plugins/fancycore/src/main/java/com/fancyinnovations/fancycore/listeners/PlayerJoinListener.java
+++ plugins/fancycore/src/main/java/com/fancyinnovations/fancycore/listeners/PlayerJoinListener.java
@@ -1,5 +1,6 @@
 package com.fancyinnovations.fancycore.listeners;
 
+import com.fancyinnovations.fancycore.api.moderation.Punishment;
 import com.fancyinnovations.fancycore.api.player.FakeHytalePlayer;
 import com.fancyinnovations.fancycore.api.player.FancyPlayer;
 import com.fancyinnovations.fancycore.main.FancyCorePlugin;
@@ -14,6 +15,14 @@ public class PlayerJoinListener {
         // TODO: use real event and register listener properly
 
         FancyPlayer fp = playerService.getByUUID(event.getPlayer().getUUID());
+        Punishment punishment = fp.isBanned();
+        if (punishment != null) {
+            event.cancel();
+            event.getPlayer().kick("You are banned from this server."); //TODO: replace with translated message (include ban reason and duration)
+            return;
+        }
+
+
         fp.setPlayer(event.getPlayer());
 
         playerService.addOnlinePlayer(fp);
@@ -24,6 +33,8 @@ public class PlayerJoinListener {
      */
     interface PlayerJoinEvent {
         @NotNull FakeHytalePlayer getPlayer();
+
+        void cancel();
     }
 
 }
