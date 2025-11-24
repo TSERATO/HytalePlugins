d7a3f5839366a4999a973046a268c4fe477111d4
Oliver <oschi1303@gmail.com>
2025-11-24 20:14:14 +0100

    fancycore: Implement player related metrics

--- plugins/fancycore/src/main/java/com/fancyinnovations/fancycore/metrics/PluginMetrics.java
+++ plugins/fancycore/src/main/java/com/fancyinnovations/fancycore/metrics/PluginMetrics.java
@@ -18,13 +18,22 @@ public class PluginMetrics {
         return plugin.getPlayerStorage().countPlayers();
     }
 
+    private double onlinePlayers() {
+        return plugin.getPlayerService().getOnlinePlayers().size();
+    }
+
     private String serverSizeCategory() {
-        // TODO: find server size category based on online players
-        // 0 = empty
-        // 1-25 = small
-        // 26-100 = medium
-        // 101-500 = large
-        // 501+ = very_large
+        if (onlinePlayers() == 0) {
+            return "empty";
+        } else if (onlinePlayers() <= 25) {
+            return "small";
+        } else if (onlinePlayers() <= 100) {
+            return "medium";
+        } else if (onlinePlayers() <= 500) {
+            return "large";
+        } else if (onlinePlayers() > 500) {
+            return "very_large";
+        }
 
         return "unknown";
     }
