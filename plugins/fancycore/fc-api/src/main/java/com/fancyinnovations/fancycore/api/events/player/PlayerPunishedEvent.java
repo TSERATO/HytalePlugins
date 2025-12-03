b5afd322481ffc0e426362708ab5b8134a1ac4f4
Oliver <oschi1303@gmail.com>
2025-12-03 13:09:33 +0100

    fancycore: Add Discord message support to PlayerPunishedEvent

--- plugins/fancycore/fc-api/src/main/java/com/fancyinnovations/fancycore/api/events/player/PlayerPunishedEvent.java
+++ plugins/fancycore/fc-api/src/main/java/com/fancyinnovations/fancycore/api/events/player/PlayerPunishedEvent.java
@@ -1,7 +1,11 @@
 package com.fancyinnovations.fancycore.api.events.player;
 
+import com.fancyinnovations.fancycore.api.discord.Embed;
+import com.fancyinnovations.fancycore.api.discord.Message;
 import com.fancyinnovations.fancycore.api.moderation.Punishment;
 import com.fancyinnovations.fancycore.api.player.FancyPlayer;
+
+import java.util.List;
 
 /**
  * Event fired when a player receives a punishment.
@@ -22,5 +26,72 @@ public class PlayerPunishedEvent extends PlayerEvent {
      */
     public Punishment getPunishment() {
         return punishment;
+    }
+
+    @Override
+    public Message getDiscordMessage() {
+        // TODO: make text translatable
+
+        switch (punishment.type()) {
+            case WARNING -> {
+                return new Message(
+                        "Player warned",
+                        List.of(
+                                new Embed(
+                                        player.getData().getUsername() + " has been warned",
+                                        "Reason: " + punishment.reason() + "\nIssued by: " +
+                                                punishment.issuedBy(),
+                                        0xdbb134
+                                )
+                        )
+                );
+            }
+
+            case MUTE -> {
+                return new Message(
+                        "Player muted",
+                        List.of(
+                                new Embed(
+                                        player.getData().getUsername() + " has been muted",
+                                        "Reason: " + punishment.reason() +
+                                                "\nIssued by: " + punishment.issuedBy() +
+                                                "\nDuration: " + (punishment.expiresAt() == -1 ? "Permanent" : ((punishment.expiresAt() - punishment.issuedAt()) / 1000) + " seconds"),
+                                        0xdbb134
+                                )
+                        )
+                );
+            }
+
+            case KICK -> {
+                return new Message(
+                        "Player kicked",
+                        List.of(
+                                new Embed(
+                                        player.getData().getUsername() + " has been kicked",
+                                        "Reason: " + punishment.reason() +
+                                                "\nIssued by: " + punishment.issuedBy(),
+                                        0xdbb134
+                                )
+                        )
+                );
+            }
+
+            case BAN -> {
+                return new Message(
+                        "Player banned",
+                        List.of(
+                                new Embed(
+                                        player.getData().getUsername() + " has been banned",
+                                        "Reason: " + punishment.reason() +
+                                                "\nIssued by: " + punishment.issuedBy() +
+                                                "\nDuration: " + (punishment.expiresAt() == -1 ? "Permanent" : ((punishment.expiresAt() - punishment.issuedAt()) / 1000) + " seconds"),
+                                        0xdbb134
+                                )
+                        )
+                );
+            }
+        }
+
+        return super.getDiscordMessage();
     }
 }
